e3ddaa42e8140af84af3bc7678d6f4b8450681d8
ejLIOLIK <smile252728@gmail.com>
2023-05-22 17:34:52 +0900

    git init with ignore

---
+++ src/main/java/com/liolik/pill2/Pill2Application.java
@@ -0,0 +1,13 @@
+package com.liolik.pill2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pill2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pill2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/liolik/pill2/Pill2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liolik.pill2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pill2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
